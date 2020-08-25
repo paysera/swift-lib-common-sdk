b5edaae8140c2391a1221695b030c2c53e9ba4e3
Pattio <Pattio@users.noreply.github.com>
2020-08-25 22:04:23 +0300

    Add PSCursors (#5)

--- Sources/Entities/PSMetadata.swift
+++ Sources/Entities/PSMetadata.swift
@@ -4,13 +4,19 @@ public class PSMetadata: Mappable {
     public var total: Int!
     public var offset: Int!
     public var limit: Int!
+    public var cursors: PSCursors?
+    public var hasNext: Bool?
+    public var hasPrevious: Bool?
     
     required public init?(map: Map) {
     }
     
     public func mapping(map: Map) {
-        total <- map["total"]
-        offset <- map["offset"]
-        limit <- map["limit"]
+        total           <- map["total"]
+        offset          <- map["offset"]
+        limit           <- map["limit"]
+        cursors         <- map["cursors"]
+        hasNext         <- map["has_next"]
+        hasPrevious     <- map["has_previous"]
     }
 }
