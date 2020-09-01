90bcdfd1ccdf8730cc29175c207f97b47db35633
Vytautas Gimbutas <v.gimbutas@paysera.com>
2020-09-01 23:13:51 +0300

    Refactored to Swift Package

--- PayseraCommonSDKTests/PayseraCommonSDKTests.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import XCTest
-import PayseraCommonSDK
-
-class PayseraCommonSDKTests: XCTestCase {
-
-}
--- Sources/Protocols/PSTokenRefresherProtocol.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import PromiseKit
-
-public protocol PSTokenRefresherProtocol {
-    func refreshToken() -> Promise<Bool>
-    func isRefreshing() -> Bool
-}
