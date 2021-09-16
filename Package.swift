dc1dafe8cb0243a67c50559bfc853a333ee9e48c
Pattio <merfisku@gmail.com>
2021-09-16 17:45:27 +0300

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -8,8 +8,8 @@ let package = Package(
         .library(name: "PayseraCommonSDK", targets: ["PayseraCommonSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", .branch("xcode13")),
-        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.1")),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.3")),
+        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.2")),
         .package(url: "https://github.com/Hearst-DD/ObjectMapper", .exact("4.2.0")),
         .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.15.3")),
     ],
