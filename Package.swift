b926fa314babdef15dc973431b99fe242788bacc
Pattio <Pattio@users.noreply.github.com>
2021-09-16 17:46:34 +0300

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -3,15 +3,15 @@ import PackageDescription
 
 let package = Package(
     name: "PayseraCommonSDK",
-    platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v9), .watchOS(.v2)],
+    platforms: [.macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6)],
     products: [
         .library(name: "PayseraCommonSDK", targets: ["PayseraCommonSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.2"),
-        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", from: "2.4.1"),
-        .package(url: "https://github.com/Hearst-DD/ObjectMapper", from: "4.2.0"),
-        .package(url: "https://github.com/mxcl/PromiseKit", from: "6.13.3"),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.3")),
+        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.2")),
+        .package(url: "https://github.com/Hearst-DD/ObjectMapper", .exact("4.2.0")),
+        .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.15.3")),
     ],
     targets: [
         .target(
