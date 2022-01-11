d43c937fb5c6647c110778e62a9d526e2c36e7c8
Pattio <merfisku@gmail.com>
2022-01-11 14:44:31 +0200

    APP-2803: Add context to mapping failure

--- Package.swift
+++ Package.swift
@@ -8,10 +8,10 @@ let package = Package(
         .library(name: "PayseraCommonSDK", targets: ["PayseraCommonSDK"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.3")),
-        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.2")),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.4")),
+        .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.3")),
         .package(url: "https://github.com/Hearst-DD/ObjectMapper", .exact("4.2.0")),
-        .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.15.3")),
+        .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.16.2")),
     ],
     targets: [
         .target(
