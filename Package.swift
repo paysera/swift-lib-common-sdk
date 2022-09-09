bd4b5091ef473ba73aca65ae5f451460f54cd7ee
Mohsen Khosravinia <khosravinia.mohsen@gmail.com>
2022-09-09 13:55:55 +0430

    Change the GitHub URL of the object mapper package.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.4.4")),
         .package(name: "JWTDecode", url: "https://github.com/auth0/JWTDecode.swift", .exact("2.6.3")),
-        .package(url: "https://github.com/Hearst-DD/ObjectMapper", .exact("4.2.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", .exact("4.2.0")),
         .package(url: "https://github.com/mxcl/PromiseKit", .exact("6.16.2")),
     ],
     targets: [
